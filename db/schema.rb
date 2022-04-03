8ab9b5966e4631507b79a82393ee15a3c86bb355
Ziminator <aleksander.zimin@gmail.com>
2022-04-03 21:42:47 +0300

    add authenticate users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_202821) do
+ActiveRecord::Schema.define(version: 2022_04_03_134201) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2022_03_31_202821) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
